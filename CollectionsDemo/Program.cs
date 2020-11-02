1591eeee592681557891bd29b6d884a6ee3aaa64
Ramya.U <ramyayadav2016@gmail.com>
2020-11-02 12:27:36 +0530

    Add project files.

---
+++ CollectionsDemo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Collections Demo!");
+            ListImplementation();
+            StackImplementation();
+            QueueImplementation();
+            DictionaryImplementation();
+        }
+
+        private static void ListImplementation()
+        {
+            Console.WriteLine("Creating a List");
+            List<string> list = new List<string>();
+
+            list.Add("Ramya");
+            list.Add("Siri");
+            list.Add("Raksh");
+
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        private static void StackImplementation()
+        {
+            Console.WriteLine("Stack implementation");
+
+            Stack<string> stack = new Stack<string>();
+
+            stack.Push("Rina");
+            stack.Push("Tina");
+            stack.Push("Mina");
+            string pop = stack.Pop();
+
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("Popped element: " + pop);
+        }
+
+        private static void QueueImplementation()
+        {
+            //creating queue
+            Queue<string> queue = new Queue<string>();
+
+            queue.Enqueue("Lily");
+            queue.Enqueue("Rose");
+            queue.Enqueue("Daisy");
+
+            foreach (var element in queue)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        private static void DictionaryImplementation()
+        {
+            //creating dictionary
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+            dictionary.Add(70, "Randy");
+            dictionary.Add(90, "Max");
+            dictionary.Add(80, "Caroline");
+
+
+        }
+    }
+}
